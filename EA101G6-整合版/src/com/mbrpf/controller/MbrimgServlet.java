a529647819925686a55fee54fb081b0567469f74
chihyang77 <chih.chih1077@gmail.com>
2020-07-02 17:21:54 +0800

    7/2

--- EA101G6-整合版/src/com/mbrpf/controller/MbrimgServlet.java
+++ EA101G6-整合版/src/com/mbrpf/controller/MbrimgServlet.java
@@ -19,22 +19,34 @@ public class MbrimgServlet extends HttpServlet {
 		res.setContentType("image/gif");
 		ServletOutputStream out = res.getOutputStream();
 
-		//讀一次後存到session裡讓效能好一點
-			HttpSession session = req.getSession();	
-			MbrpfVO mbrpfVO = null;
-			String mbrno = req.getParameter("mbrno");
-			System.out.println(mbrno);
-			if(session.getAttribute(mbrno)==null) {
-				MbrpfService ser = new MbrpfService();
-				mbrpfVO = ser.getOneMbrpf(mbrno);
-				out.write(mbrpfVO.getMbrimg());
-				session.setAttribute(mbrno, mbrpfVO);
-			}else {
-				mbrpfVO=(MbrpfVO)session.getAttribute(mbrno);
-				out.write(mbrpfVO.getMbrimg());
-			}
+//		//讀一次後存到session裡讓效能好一點
+//			HttpSession session = req.getSession();	
+//			MbrpfVO mbrpfVO = null;
+//			String mbrno = req.getParameter("mbrno");
+//			System.out.println(mbrno);
+//			if(session.getAttribute(mbrno)==null) {
+//				MbrpfService ser = new MbrpfService();
+//				mbrpfVO = ser.getOneMbrpf(mbrno);
+//				out.write(mbrpfVO.getMbrimg());
+//				session.setAttribute(mbrno, mbrpfVO);
+//			}else {
+//				mbrpfVO=(MbrpfVO)session.getAttribute(mbrno);
+//				out.write(mbrpfVO.getMbrimg());
+//			}
 
-			
+		try {
+			String mbrno = req.getParameter("mbrno");
+			MbrpfService mbrpfSvc = new MbrpfService();
+			MbrpfVO mbrpfVO = mbrpfSvc.getOneMbrpf(mbrno);
+			byte[] buf = mbrpfVO.getMbrimg();
+			out.write(buf);
+		} catch (Exception e) {
+			InputStream in = getServletContext().getResourceAsStream("/front-end/images/back1.gif");
+			byte[] b = new byte[in.available()];
+			in.read(b);
+			out.write(b);
+			in.close();
+		}
 			
 			
 		}
